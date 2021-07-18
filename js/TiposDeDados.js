86ce252a019d67c9fe98ad6314c3322bea2a51c3
jvitorsouza <vitor3souza@gmail.com>
2021-07-18 20:30:03 -0300

    mais um dia de estudos

---
+++ js/TiposDeDados.js
@@ -0,0 +1,58 @@
+  // ""as duplas
+ //''aspas simples
+ //`` template literals
+
+ console.log(1+1)// permite colocar expressoes dentro da string
+ console.log("vitor ${1+1}")// isso é chamado de interpolacao
+ 
+ /*number 
+      numeros
+      33//inteiros
+      12.4 // reais - float
+      naN // not a  number
+      infinity // infinito
+      */
+     console.log (12.5/4)
+
+     //boolean
+     console.log(false)
+
+
+/*
+  *undefined 
+      *indefunido
+   
+  * null    
+      *nulo
+      *objeto que nao possui nada dentro
+      *diferente de indefinido
+
+*/
+/*Object 
+  * Objeto 
+  * Propriedades / Atributos
+  * Funcionalidades / Métodos
+  
+{ propriedade: "Valor"}
+*/
+console.log({
+  name: "vitor",
+  idade: 24,
+  andar: function(){
+    console.log ('andar')
+  }
+
+})
+
+/*
+  * Array (vetores)
+  *uma lista 
+  * Agrupamento de dado
+  * ["vitor", 24]
+  */
+  console.log([
+    "leite", 
+    "ovos",
+    2,
+    3
+  ])
---
+++ js/comentarios.js
@@ -0,0 +1,8 @@
+//aula de adicionar comentarios em linha!
+// o codigo abaixo escreve alguma coisa no console
+ /*aqui eu tenho um comentario mult linha
+ oojdfndfndsfsdfds[]
+ */
+
+console.log('Bem vindos ao Starter')
+console.log('teste')
